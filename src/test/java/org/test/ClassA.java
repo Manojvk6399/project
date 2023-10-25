061d4fe467f2d907eef16eb29fd7af45b73abe96
GITHUB <manojvk97@gmail.com>
2023-10-25 21:56:58 +0530

    Main Task

---
+++ src/test/java/org/test/ClassA.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class ClassA {
+	private void method1() {
+		System.out.println(10);
+	}
+	private void method2() {
+		System.out.println(20);
+	}
+	private void method3() {
+		System.out.println(30);
+	}
+}
