c2a315dfaf9c01b8dffefcc974cea1b2702a3965
GITHUB <manojvk97@gmail.com>
2023-10-25 22:42:48 +0530

    Add method

--- src/test/java/org/test/ClassA.java
+++ src/test/java/org/test/ClassA.java
@@ -10,4 +10,13 @@ public class ClassA {
 	private void method3() {
 		System.out.println(30);
 	}
+	private void test1() {
+		System.out.println(1);
+	}
+	private void test2() {
+		System.out.println(2);
+	}
+	private void test3() {
+		System.out.println(3);
+	}
 }
